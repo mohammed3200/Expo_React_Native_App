2aaf791953d7673b0b9a5eacfb47b5f11b71bda8
mohammed3200 <85238689+mohammed3200@users.noreply.github.com>
2024-03-24 17:09:03 +0200

    The first step is to fetch access_token

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const API_URL = 'http://results.justhost.ly/auth/signin';
+  const [RegNo, setRegNo] = useState('112181502');
+  const [Password, setPassword] = useState('6504104');
+  const [Token, setToken] = useState(null || '');
+
+  useEffect(() => {
+    const fetchData = async () => {
+       try {
+         const response = await axios.post(API_URL, {
+           RegNo,
+           Password,
+         });
+         setToken(response.data.access_token);
+       } catch (error) {
+         console.warn(error);
+       }
+    };
+   
+    fetchData();
+   }, []);
+   
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Access Token: {Token}</Text>
       <StatusBar style="auto" />
     </View>
   );
